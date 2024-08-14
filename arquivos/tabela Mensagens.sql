1d00b54aa7ef522add58d7342b76f4f051b9a35e
Larissa Cavalcante <112430561+larissa-sc@users.noreply.github.com>
2024-08-14 16:37:00 -0300

    Cópia de segurança Projeto AMAS

---
+++ arquivos/tabela Mensagens.sql
@@ -0,0 +1,12 @@
+Create DATABASE Formularioo;
+
+USE Formularioo;
+
+Create Table Mensagens(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Nome Varchar (50),
+    Email Varchar (100),
+    Telefone Varchar(70),
+    Cidade Varchar (80),
+    Mensagens text
+);
