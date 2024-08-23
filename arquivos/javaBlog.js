2a71306cb7fe60904bd8c8f6fa36b6b783c55329
Levi Rodrigues <150200863+LeviRodrigues22@users.noreply.github.com>
2024-08-23 19:54:21 -0300

    Add files via upload

--- arquivos/javaBlog.js
+++ arquivos/javaBlog.js
@@ -1,20 +1,28 @@
+// Adiciona um evento que será executado quando o conteúdo do DOM for completamente carregado
 document.addEventListener('DOMContentLoaded', () => {
+     // Obtém o formulário com o ID 'newNewsForm'
     const form = document.getElementById('newNewsForm');
+     // Obtém o elemento div onde as notícias serão adicionadas
     const newsDiv = document.getElementById('news');
 
+    // Adiciona um evento de submissão ao formulário
     form.addEventListener('submit', (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Previne o comportamento padrão de submissão do formulário
 
+           // Obtém os valores dos campos do formulário
         const title = document.getElementById('newsTitle').value;
         const link = document.getElementById('newsLink').value;
         const image = document.getElementById('newsImage').value;
 
+         // Verifica se todos os campos estão preenchidos
         if (title && link && image) {
+            // Cria um novo elemento div para a notícia
             const newsItem = document.createElement('div');
-            newsItem.classList.add('news-item');
+            newsItem.classList.add('news-item'); // Adiciona a classe 'news-item' ao div
 
+             // Cria um novo elemento de link
             const newsLink = document.createElement('a');
-            newsLink.href = link;
+            newsLink.href = link; //define href do link
             newsLink.target = '_blank';
 
             const newsImage = document.createElement('img');
@@ -24,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const newsTitle = document.createElement('h4');
             newsTitle.textContent = title;
 
+            // Adiciona a imagem e o título ao link
             newsLink.appendChild(newsImage);
             newsLink.appendChild(newsTitle);
             newsItem.appendChild(newsLink);
--- arquivos/paginas/scriptForms.js
+++ arquivos/paginas/scriptForms.js
@@ -1,23 +1,30 @@
+// URL da API Supabase para onde as mensagens serão enviadas
 const URL_SUPA = "https://dcofwjpqqjsvzesqpyuc.supabase.co/rest/v1/mensagens";
+
+// Cabeçalhos necessários para a requisição, incluindo a chave da API e o token de autorização
 const HEADERS = {
     "Content-Type":"application/json",
     'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjb2Z3anBxcWpzdnplc3FweXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQzNjc5NzEsImV4cCI6MjAzOTk0Mzk3MX0.KISQ2_J_azg2gjjQ-o0mM3RULkSF-uGv8yy2FULmnJM',
     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjb2Z3anBxcWpzdnplc3FweXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQzNjc5NzEsImV4cCI6MjAzOTk0Mzk3MX0.KISQ2_J_azg2gjjQ-o0mM3RULkSF-uGv8yy2FULmnJM'
 }
+// Adiciona um evento ao carregar o conteúdo da página
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('contactForm');
 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
 
+    // Obtém os valores dos campos do formulário
     const nome = form.nome.value.trim();
     const email = form.email.value.trim();
     const telefone = form.telefone.value.trim();
     const cidade = form.cidade.value.trim();
     const mensagem = form.mensagem.value.trim();
-
+    
+    // Verifica se todos os campos estão preenchidos
     if (nome && email && telefone && cidade && mensagem) {
-      // Enviar os dados para o servidor mockado usando fetch
+
+      // Envia os dados para o servidor usando fetch
       fetch(URL_SUPA, {
         method: 'POST',
         headers: HEADERS,
@@ -31,11 +38,11 @@ document.addEventListener('DOMContentLoaded', function () {
       })
         .then(data => {
           console.log('Sucesso:', data);
-          alert('Mensagem enviada com sucesso!');
-          form.reset();
+          alert('Mensagem enviada com sucesso!'); //alerta de sucesso
+          form.reset(); //reseta o formulário
         })
         .catch(error => {
-          console.error('Erro:', error);
+          console.error('Erro:', error); //loga o erro no console
           alert('Ocorreu um erro ao enviar a mensagem.');
         });
     } else {
@@ -43,7 +50,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
-
+  
+// Função assíncrona para obter a lista de mensagens
 const getLista = async () => {
   try {
     const response = await fetch(URL_SUPA + '?order=id.desc&limit=10', {
@@ -52,16 +60,17 @@ const getLista = async () => {
       }
     );
     if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+      throw new Error(`Response status: ${response.status}`); // Lança um erro se a resposta não for ok
     }
-    const json = await response.json();
-    adicionaTabela(json);
+    const json = await response.json(); //converte a reposta para JSON
+    adicionaTabela(json); // add dados á tabela
 
   } catch (error) {
       console.error(error.message);
   }
 }
 
+// Função para adicionar os dados à tabela
 const adicionaTabela = (data) => {
   const tableBody = document.getElementById('nomes').querySelector('tbody');
   tableBody.innerHTML = ''; // Limpa a tabela antes de adicionar os dados
@@ -70,10 +79,11 @@ const adicionaTabela = (data) => {
     const row = document.createElement('tr');
 
     const nomeCell = document.createElement('td');
-    nomeCell.textContent = item.nome;
+    nomeCell.textContent = item.nome; // Adiciona o nome à célula
     row.appendChild(nomeCell);
 
-    tableBody.appendChild(row);
+    tableBody.appendChild(row); // Adiciona a linha à tabela
   });
 }
+// Chama a função getLista quando a página é carregada
 window.onload = () => {getLista()}
